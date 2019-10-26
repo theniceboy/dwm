b18bb40525707f2979fae40127351faa0a726f39
David Chen <weichen2000121@gmail.com>
2019-10-26 11:04:16 -0700

    autostart patch

--- dwm.c
+++ dwm.c
@@ -195,6 +195,7 @@ static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
 static void run(void);
+static void runAutostart(void);
 static void scan(void);
 static int sendevent(Client *c, Atom proto);
 static void sendmon(Client *c, Monitor *m);
@@ -1389,6 +1390,12 @@ run(void)
 }
 
 void
+runAutostart(void) {
+	system("cd ~/.config/dwm; ./autostart_blocking.sh");
+	system("cd ~/.config/dwm; ./autostart.sh &");
+}
+
+void
 scan(void)
 {
 	unsigned int i, num;
@@ -2190,6 +2197,7 @@ main(int argc, char *argv[])
 		die("pledge");
 #endif /* __OpenBSD__ */
 	scan();
+	runAutostart();
 	run();
 	cleanup();
 	XCloseDisplay(dpy);
