b913d619fa356487afbf1672381e824c895749d7
David Chen <weichen2000121@gmail.com>
2019-10-26 10:57:03 -0700

    changed config.mk

--- config.mk
+++ config.mk
@@ -7,8 +7,8 @@ VERSION = 6.2
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/X11
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
